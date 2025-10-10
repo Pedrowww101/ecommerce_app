63582199a68e18247b146b5b796c783e91c7a3e9
Pedrowww101 <bondadpetermaironne@gmail.com>
2025-10-10 22:20:35 +0800

    fix: convert pagination query params from string to number

--- src/controller/products/get-all-products.controller.ts
+++ src/controller/products/get-all-products.controller.ts
@@ -5,18 +5,23 @@ import { ProductRepository } from "../../repositories/products/product.repositor
 import { ProductService } from "../../services/product.service.js";
 
 const querySchema = type({
-   page: "number",
-   limit: "number",
+   page: "string",
+   limit: "string",
 });
 export const getAllProductsController = factory.createHandlers(
    validator("query", querySchema),
    async (c) => {
       const params = c.req.valid("query");
 
+      const queryNumber = {
+         page: Number(params.page),
+         limit: Number(params.limit),
+      };
+
       const productRepo = new ProductRepository();
       const productService = new ProductService(productRepo);
 
-      const result = await productService.getAllProducts(params);
+      const result = await productService.getAllProducts(queryNumber);
 
       return c.json(result, 200);
    }
