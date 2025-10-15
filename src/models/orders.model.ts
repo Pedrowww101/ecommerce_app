4c282016a515c4bd2f7b7ba64355cb38ea1b21cb
Pedrowww101 <bondadpetermaironne@gmail.com>
2025-10-15 18:47:15 +0800

    add: simple orders model

---
+++ src/models/orders.model.ts
@@ -0,0 +1,10 @@
+import {
+   createInsertSchema,
+   createUpdateSchema,
+   createSelectSchema,
+} from "drizzle-arktype";
+import { orders } from "../database/app-schema.js";
+
+export const ordersSchema = createSelectSchema(orders);
+export const insertOrdersSchema = createInsertSchema(orders);
+export const updateOrdersSchema = createUpdateSchema(orders);
