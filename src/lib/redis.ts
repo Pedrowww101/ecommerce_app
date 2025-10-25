3bd3a1e35ba0e8d866f84ad5be9348c4690d1173
Pedrowww101 <bondadpetermaironne@gmail.com>
2025-10-25 15:55:45 +0800

    refactor(redis): simplify Redis connection setup by using REDIS_URL
    - Removed hardcoded connection parameters and replaced them with a single REDIS_URL environment variable for configuration.
    - Streamlined the Redis client initialization process for better maintainability.

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,7 @@
 import { createClient } from "redis";
 
 export const redis = createClient({
-   username: "default",
-   password: "OBlDbbAZYQD2zLGqXCmeN7DrCzxRZTIN",
-   socket: {
-      host: "redis-11773.crce178.ap-east-1-1.ec2.redns.redis-cloud.com",
-      port: 11773,
-   },
+   url: process.env.REDIS_URL,
 });
 
 redis.on("error", (err) => console.log("Redis Client Error", err));
