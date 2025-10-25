99413d369719028ad665ca60500a383578761df2
Pedrowww101 <bondadpetermaironne@gmail.com>
2025-10-25 18:44:18 +0800

    feat(products): add update product route with role-based access
    - Introduced a PATCH route for updating products, secured with role and permission middleware.
    - Integrated updateProductController to handle product updates based on user permissions.

--- src/routes/protected/product.routes.ts
+++ src/routes/protected/product.routes.ts
@@ -3,12 +3,18 @@ import { Env } from "../../lib/auth.type.js";
 import { addProductController } from "../../controller/products/add-product.controller.js";
 import { roleAndPermissionMiddleware } from "../../middleware/role-permission.middleware.js";
 import { getAllLowStocksProductController } from "../../controller/products/get-all-low-stocks-product.controller.js";
+import { updateProductController } from "../../controller/products/update-product.controller.js";
 
 const protectedProductRoutes = new Hono<Env>()
    .post(
       "/add",
       roleAndPermissionMiddleware("product", "create"),
       ...addProductController
+   )
+   .patch(
+      "/:id",
+      roleAndPermissionMiddleware("product", "update"),
+      ...updateProductController
    )
    .get(
       "/low-stock",
