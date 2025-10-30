69a730f89bab435bb9d052ff43929ba7726072bf
Pedrowww101 <bondadpetermaironne@gmail.com>
2025-10-30 13:15:39 +0800

    feat(database): introduce comprehensive schema for e-commerce platform
    - Added a new SQL migration file to define the complete schema for the e-commerce platform, including tables for addresses, carts, cart items, categories, orders, products, users, and more.
    - Established relationships between tables through foreign keys to ensure data integrity and facilitate complex queries.
    - Created an ENUM type for order statuses to standardize order management.
    - Removed outdated migration files to streamline the database structure and improve maintainability.

--- src/database/drizzle/0002_tearful_namor.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "cart_items" ADD COLUMN "unit_price" numeric(10, 2) NOT NULL;--> statement-breakpoint
-ALTER TABLE "cart_items" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "cart_items" ADD COLUMN "updated_at" timestamp DEFAULT now() NOT NULL;
